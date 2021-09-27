a17d68677ee8133ee02de6ac8539a96e1b302b9b
leonardoromero <leonardorommero@gmail.com>
2021-09-27 00:51:30 -0300

    segunda entrega del proyecto final

--- javascript/script.js
+++ javascript/script.js
@@ -6,68 +6,11 @@ class Servicio {
         this.dias = dias;
         this.precio = precio;
     }
-    presupuesto() {
-        const checkOutNombres = [];
-        let checkOutDias = 0;
-        let checkOutPrecio = 0;
-        let pedido = prompt(`Para presupuestar su proyecto, ingrese los códigos identificadores de los servicios deseados: \n\
-        1. ${servicios[0].nombre} \n\
-        2. ${servicios[1].nombre} \n\
-        3. ${servicios[2].nombre} \n\
-        4. ${servicios[3].nombre} \n\
-        5. ${servicios[4].nombre} \n\
-        6. ${servicios[5].nombre} \n\
-        `);
-                   
-        for(let i = 0; i < pedido.length; i++){
-            switch(pedido.charAt(i)){
-                case '1': 
-                    checkOutNombres.push('✅ ' + servicios[0].nombre);
-                    checkOutDias += servicios[0].dias;
-                    checkOutPrecio += servicios[0].precio;
-                    break;
-                case '2': 
-                    checkOutNombres.push('✅ ' + servicios[1].nombre);
-                    checkOutDias += servicios[1].dias;
-                    checkOutPrecio += servicios[1].precio;
-                    break;
-                case '3': 
-                    checkOutNombres.push('✅ ' + servicios[2].nombre);
-                    checkOutDias += servicios[2].dias;
-                    checkOutPrecio += servicios[2].precio;
-                    break;
-                case '4': 
-                    checkOutNombres.push('✅ ' + servicios[3].nombre);
-                    checkOutDias += servicios[3].dias;
-                    checkOutPrecio += servicios[3].precio;
-                    break;
-                case '5': 
-                    checkOutNombres.push('✅ ' + servicios[4].nombre);
-                    checkOutDias += servicios[4].dias;
-                    checkOutPrecio += servicios[4].precio;
-                    break;
-                case '6': 
-                    checkOutNombres.push('✅ ' + servicios[5].nombre);
-                    checkOutDias += servicios[5].dias;
-                    checkOutPrecio += servicios[5].precio;
-                    break;
-            }            
-        }
-
-        if(checkOutNombres != ''){
-            alert(`Usted eligió los siguientes servicios: \n\n${checkOutNombres.join('\n')}\n\n \
-            El total de su proyecto es de $${checkOutPrecio} y llevará aproximadamente ${checkOutDias} dias.`);
-        } else {
-            alert('Ingrese un código válido')
-            this.presupuesto();
-        }
-
-    }
 }
 
 const disenio = new Servicio(1, 'Diseño', 'Diseño web integral sin maquetado', 7, 10000);
 const maquetado = new Servicio(2, 'Desarrollo', 'Maquetado (sin diseño)', 21, 10000);
-const desarrollo = new Servicio(3, 'Desarrollo', 'Diseño y maquetado', 30, 18000);
+const desarrollo = new Servicio(3, 'Desarrollo', '⚡ Diseño y maquetado', 30, 18000);
 const hosting = new Servicio(4, 'Desarrollo', 'Trámites de creación del dominio + cloud hosting', 4, 4000);
 const logo = new Servicio(5, 'Diseño', 'Diseño de logo', 3, 4000);
 const canva = new Servicio(6, 'Diseño', 'Diseño de banners y flyers multiplataforma', 7, 8000);
@@ -75,15 +18,17 @@ const mantenimiento = new Servicio(7, 'Mantenimiento', 'Mantenimiento mensual', 
 
 const servicios = [disenio, maquetado, desarrollo, hosting, logo, canva, mantenimiento];
 
+// Se crea un div por cada elemento del array servicios
 for(let servicio of servicios){
     let item = document.createElement('div');
-    item.className = `presupuestador__servicio presupuestador__servicio--${servicio.codigo} presupuestador__servicio--not-selected`;
+    item.className = `presupuestador__servicio presupuestador__servicio--${servicio.codigo}`;
     item.innerHTML = `<div class="presupuestador__servicio--titulo">${servicio.nombre}</div>
                     <div class="presupuestador__servicio--precio">$${servicio.precio}</div>`;
     document.getElementById('servicios').appendChild(item);
 
 }
 
+// DOM
 let servicio1 = document.getElementById("servicios").children[0];
 let servicio2 = document.getElementById("servicios").children[1];
 let servicio3 = document.getElementById("servicios").children[2];
@@ -92,35 +37,142 @@ let servicio5 = document.getElementById("servicios").children[4];
 let servicio6 = document.getElementById("servicios").children[5];
 let servicio7 = document.getElementById("servicios").children[6];
 
+let total = 0;
+
+// Event listeners para agregar al presupuesto cada servicio seleccionado, que a su vez se almacena en localStorage
 servicio1.onclick = () => {
     servicio1.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[0].nombre);
+    total += servicios[0].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-1`;
+    document.getElementById('item-1').innerHTML = `<div class="presupuesto__items--titulo">${servicios[0].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[0].precio}</div>`;
+
+    localStorage.setItem('item1', JSON.stringify(servicios[0]));
 };
+
 servicio2.onclick = () => {
     servicio2.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[1].nombre);
+    total += servicios[1].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-2`;
+    document.getElementById('item-2').innerHTML = `<div class="presupuesto__items--titulo">${servicios[1].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[1].precio}</div>`;
+
+    localStorage.setItem('item2', JSON.stringify(servicios[1]));
 };
+
 servicio3.onclick = () => {
     servicio3.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[2].nombre);
+    total += servicios[2].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-3`;
+    document.getElementById('item-3').innerHTML = `<div class="presupuesto__items--titulo">${servicios[2].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[2].precio}</div>`;
+
+    localStorage.setItem('item3', JSON.stringify(servicios[2]));
 };
+
 servicio4.onclick = () => {
     servicio4.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[3].nombre);
+    total += servicios[3].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-4`;
+    document.getElementById('item-4').innerHTML = `<div class="presupuesto__items--titulo">${servicios[3].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[3].precio}</div>`;
+
+    localStorage.setItem('item4', JSON.stringify(servicios[3]));
 };
+
 servicio5.onclick = () => {
     servicio5.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[4].nombre);
+    total += servicios[4].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-5`;
+    document.getElementById('item-5').innerHTML = `<div class="presupuesto__items--titulo">${servicios[4].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[4].precio}</div>`;
+
+    localStorage.setItem('item5', JSON.stringify(servicios[4]));
 };
+
 servicio6.onclick = () => {
     servicio6.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[5].nombre);
+    total += servicios[5].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-6`;
+    document.getElementById('item-6').innerHTML = `<div class="presupuesto__items--titulo">${servicios[5].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[5].precio}</div>`;
+
+    localStorage.setItem('item6', JSON.stringify(servicios[5]));
 };
+
 servicio7.onclick = () => {
     servicio7.classList.toggle('presupuestador__servicio--selected');
-    carrito.push(servicios[6].nombre);
+    total += servicios[6].precio;
+    document.getElementById('total').innerHTML = `Total $${total}`;
+
+    document.getElementById('presupuesto').style.display = 'block';
+    
+    let listaItems = document.getElementById('presupuesto__items');
+    let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+    itemPresupuesto.id = `item-7`;
+    document.getElementById('item-7').innerHTML = `<div class="presupuesto__items--titulo">${servicios[6].nombre}</div>
+                                                <div class="presupuesto__items--precio">$${servicios[6].precio}</div>`;
+
+    localStorage.setItem('item7', JSON.stringify(servicios[6]));
 };
 
-const carrito = [];
-console.log(carrito);
+// Comprueba si localStorage contiene elementos y, de ser así, los mantiene en el presupuesto
+if(localStorage.length != 0){
+    document.getElementById('presupuesto').style.display = 'block';
+    for(let i = 0; i < localStorage.length; i++){
+        console.log(JSON.parse(localStorage.getItem(localStorage.key(0))).nombre);
+        let clave = localStorage.key(i);
+        let listaItems = document.getElementById('presupuesto__items');
+        let itemPresupuesto = listaItems.appendChild(document.createElement('div'));
+        itemPresupuesto.id = `item-${i}`;
+        document.getElementById(`item-${i}`).innerHTML = `<div class="presupuesto__items--titulo">${JSON.parse(localStorage.getItem(localStorage.key(i))).nombre}</div>
+                                                <div class="presupuesto__items--precio">$${JSON.parse(localStorage.getItem(localStorage.key(i))).precio}</div>`;
 
+        total += JSON.parse(localStorage.getItem(localStorage.key(i))).precio;
+        document.getElementById('total').innerHTML = `Total $${total}`;
+    }
+} else {
+    document.getElementById('presupuesto').style.display = 'none';
+}
+
+// Borra el localStorage al presional el botón
+document.getElementById('clear-btn').onclick = () => {
+    localStorage.clear();
+    location.reload();
+}
