50b31eaf88f9234b740494f786144e98e8848873
leonardoromero <leonardorommero@gmail.com>
2021-10-11 19:28:10 -0300

    entrega complementaria de efectos concatenados junto con correcciones en el algoritmo en base a las observaciones de Ricardo

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,25 @@
 class Servicio {
-    constructor(codigo, tipo, nombre, dias, precio) {
+    constructor(codigo, tipo, nombre, dias, precio, cantidad) {
         this.codigo = codigo;
         this.tipo = tipo;
         this.nombre = nombre;
         this.dias = dias;
         this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    sumar = () => {
+        this.cantidad++;
     }
 }
 
-const disenio = new Servicio(1, 'Diseño', 'Diseño web integral sin maquetado', 7, 10000);
-const maquetado = new Servicio(2, 'Desarrollo', 'Maquetado (sin diseño)', 21, 10000);
-const desarrollo = new Servicio(3, 'Desarrollo', '⚡ Diseño y maquetado', 30, 18000);
-const hosting = new Servicio(4, 'Desarrollo', 'Trámites de creación del dominio + cloud hosting', 4, 4000);
-const logo = new Servicio(5, 'Diseño', 'Diseño de logo', 3, 4000);
-const canva = new Servicio(6, 'Diseño', 'Diseño de banners y flyers multiplataforma', 7, 8000);
-const mantenimiento = new Servicio(7, 'Mantenimiento', 'Mantenimiento mensual', 0, 1500);
+const disenio = new Servicio(1, 'Diseño', 'Diseño web integral sin maquetado', 7, 10000, 0);
+const maquetado = new Servicio(2, 'Desarrollo', 'Maquetado (sin diseño)', 21, 10000, 0);
+const desarrollo = new Servicio(3, 'Desarrollo', '⚡ Diseño y maquetado', 30, 18000, 0);
+const hosting = new Servicio(4, 'Desarrollo', 'Trámites de creación del dominio + cloud hosting', 4, 4000, 0);
+const logo = new Servicio(5, 'Diseño', 'Diseño de logo', 3, 4000, 0);
+const canva = new Servicio(6, 'Diseño', 'Diseño de banners y flyers multiplataforma', 7, 8000, 0);
+const mantenimiento = new Servicio(7, 'Mantenimiento', 'Mantenimiento mensual', 0, 1500, 0);
 
 const servicios = [disenio, maquetado, desarrollo, hosting, logo, canva, mantenimiento];
 
@@ -22,47 +27,76 @@ const servicios = [disenio, maquetado, desarrollo, hosting, logo, canva, manteni
 for(let servicio of servicios){
     $("#servicios").append(`<div class="presupuestador__servicio" id="presupuestador__servicio--${servicio.codigo}">
                                 <div class="presupuestador__servicio--titulo">${servicio.nombre}</div>
-                                <div class="presupuestador__servicio--precio">$${servicio.precio}</div>
+                                <button class="btn-secondary btn" id="agregar-${servicio.codigo}">Agregar</button>
                             </div>`);
-
 }
 
+// Setea al total del presupuesto en 0
 let total = 0;
 
-// Event listeners para agregar al presupuesto cada servicio seleccionado, que a su vez se almacena en localStorage
-$('#servicios').on('click', (e) => {
-    localStorage.setItem(`item${localStorage.length}`, JSON.stringify(servicios[(e.target.id).slice(-1)-1]));
+// Setea todos los elementos del presupuesto y solo los muestra si su cantidad es mayor que cero
+for(let i= 0; i< servicios.length; i++){
+    $(`#presupuesto__items--cantidad-${i}`).html(`${servicios[i].cantidad}`);
+    $(`#presupuesto__items--titulo-${i}`).html(`${servicios[i].nombre}`);
+    $(`#presupuesto__items--precio-${i}`).html(`$${servicios[i].precio}`);
+}
 
-    $(`${e.target.id}`).toggleClass('presupuestador__servicio--selected');
-    total += parseInt((e.target.children[1].innerHTML).slice(1));
+// Event listener para mostrar en el presupuesto cada servicio seleccionado, y a su vez los almacena en el localStorage. 
+// Si ya existe en el localStorage solo le suma a propiedad cantidad
+
+$('.btn-secondary').on('click', (e) => {
+    // Extrae el código de producto del id que encuentra en el target del evento
+    let index = (e.target.id).slice(-1)-1;
+
+    total += servicios[index].precio;
+
+    // Muestra en el presupuesto los elementos seleccionados
+    $(`#presupuesto__items__item-${index}`).css('display', 'flex');
+    $(`#presupuesto__items__item-${index}`).fadeOut(100);
+    $(`#presupuesto__items__item-${index}`).fadeIn(300);
+
+    // Si el elemento no se encuentra en el localStorage lo crea, visibilizando sus propiedades, y si ya existe lo actualiza
+    if(!localStorage.getItem(`item${index}`)){
+        servicios[index].sumar();
+        $(`#presupuesto__items--precio-${index}`).html(`$${servicios[index].cantidad * `${servicios[index].precio}`}`);
+        $(`#presupuesto__items--cantidad-${index}`).html(`${servicios[index].cantidad}`);
+        localStorage.setItem(`item${index}`, JSON.stringify(servicios[index]));
+    } else {
+        let dataStorage = JSON.parse(localStorage.getItem(`item${index}`));
+        dataStorage.cantidad = dataStorage.cantidad + 1;
+        localStorage.setItem(`item${index}`, JSON.stringify(dataStorage));
+        $(`#presupuesto__items--cantidad-${index}`).html(`${JSON.parse(localStorage.getItem(`item${index}`)).cantidad}`);
+        $(`#presupuesto__items--precio-${index}`).html(`$${JSON.parse(localStorage.getItem(`item${index}`)).cantidad * JSON.parse(localStorage.getItem(`item${index}`)).precio}`);
+    }
+
     $('#total').html(`Total $${total}`);
-    $('#presupuesto').css({display: "block"});
-    
-    $('#presupuesto__items').append(`<div id="item-${localStorage.length-1}">
-                                        <div class="presupuesto__items--titulo">${e.target.firstElementChild.innerHTML}</div>
-                                        <div class="presupuesto__items--precio">${e.target.lastElementChild.innerHTML}</div>
-                                    </div>`)
+    $('#presupuesto').slideDown(500);
 })
 
 // Comprueba si localStorage contiene elementos y, de ser así, los mantiene en el presupuesto
 if(localStorage.length != 0){
-    $('#presupuesto').css({display: "block"});
+    $('#presupuesto').show();
     for(let i = 0; i < localStorage.length; i++){
         let clave = localStorage.key(i);
         
-        $('#presupuesto__items').append(`<div id="item-${i}">
-                                            <div class="presupuesto__items--titulo">${JSON.parse(localStorage.getItem(clave)).nombre}</div>
-                                            <div class="presupuesto__items--precio">$${JSON.parse(localStorage.getItem(clave)).precio}</div>
-                                        </div>`);
-        total += JSON.parse(localStorage.getItem(clave)).precio;
+        total += JSON.parse(localStorage.getItem(clave)).precio * JSON.parse(localStorage.getItem(clave)).cantidad;
         $('#total').html(`Total $${total}`);
+
+        $(`#presupuesto__items__item-${i}`).css('display', 'flex');
+        $(`#presupuesto__items--cantidad-${i}`).html(`${JSON.parse(localStorage.getItem(clave)).cantidad}`);
+        $(`#presupuesto__items--precio-${i}`).html(`$${JSON.parse(localStorage.getItem(clave)).cantidad * JSON.parse(localStorage.getItem(clave)).precio}`);
+    
     }
 } else {
-    $('#presupuesto').css({display: "none"});
+    $('#presupuesto').hide();
 }
 
-// Borra el localStorage al presional el botón
+// El botón borra el localStorage, recarga la página y setea la cantidad de los productos en 0
 $('#clear-btn').on('click', () => {
     localStorage.clear();
     location.reload();
+    for(let i = 0; i < servicios.length; i++){
+        servicios[i].cantidad = 0;
+    }
 });
+
